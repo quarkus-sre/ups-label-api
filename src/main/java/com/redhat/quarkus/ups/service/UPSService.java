84fe1178ee9448c2df133d330217b62fa1b9e085
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-06-22 11:14:15 -0300

    delay

--- src/main/java/com/redhat/quarkus/ups/service/UPSService.java
+++ src/main/java/com/redhat/quarkus/ups/service/UPSService.java
@@ -3,11 +3,13 @@ package com.redhat.quarkus.ups.service;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import com.redhat.quarkus.ups.domain.Label;
 import com.redhat.quarkus.ups.domain.Order;
@@ -15,12 +17,16 @@ import com.redhat.quarkus.ups.domain.Order;
 @Path("/ups")
 public class UPSService {
 
+  @ConfigProperty(name="label.delay", defaultValue = "10000")
+  long delay;
+
   @POST
   @Path("/createlabel")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public Label createLabel(Order order) {
+  public Label createLabel(Order order) throws InterruptedException {
     System.out.println("Label generated -> "+UUID.randomUUID().toString());
+    Thread.sleep(delay);
     return new Label(UUID.randomUUID().toString(), LocalDate.now());
   }
 
