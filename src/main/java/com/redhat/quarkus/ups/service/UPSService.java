34b8c99a37b55ab915088693a74dafda24fcda60
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-07-01 10:46:11 -0300

    corrigindo

--- src/main/java/com/redhat/quarkus/ups/service/UPSService.java
+++ src/main/java/com/redhat/quarkus/ups/service/UPSService.java
@@ -1,6 +1,7 @@
 package com.redhat.quarkus.ups.service;
 
 import java.time.LocalDate;
+import java.util.Random;
 import java.util.UUID;
 
 import javax.ws.rs.Consumes;
@@ -9,30 +10,28 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import com.redhat.quarkus.ups.domain.Label;
 import com.redhat.quarkus.ups.domain.Order;
 
 @Path("/ups")
 public class UPSService {
 
-  @ConfigProperty(name="label.delay", defaultValue = "2000")
-  long delay;
-
   int minDelay = 300;
   int maxDelay = 1000;
+
+  Random random = new Random();
   
   @POST
   @Path("/createlabel")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
   public Label createLabel(Order order) throws InterruptedException {
-    System.out.println("Label generated -> "+UUID.randomUUID().toString());
-    delay = (int) ((Math.random() * (maxDelay - minDelay)) + minDelay);
+    UUID randomUUID = UUID.randomUUID();
+    System.out.println("Label generated -> "+randomUUID.toString());
+    int delay = random.nextInt(maxDelay - minDelay) + minDelay;
     System.out.println("UPS Delay -> "+ delay);
     Thread.sleep(delay);
-    return new Label(UUID.randomUUID().toString(), LocalDate.now());
+    return new Label(randomUUID.toString(), LocalDate.now());
   }
 
 }
